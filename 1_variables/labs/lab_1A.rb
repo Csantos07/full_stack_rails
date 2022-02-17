de4865efaeef2a8f861854e7199d6334f69b8bca
Carlo Santos <carlo@snapit.solutions>
2022-02-17 09:22:22 -0600

    begin lab answers'

---
+++ 1_variables/labs/lab_1A.rb
@@ -0,0 +1,14 @@
+# Lab 1A
+# -------
+#
+# Write a small program to calculate how many minutes there are in a year. 
+# Feel free to google the formula if you need help getting started.
+
+minutes_in_a_year = 365 * 24 * 60
+
+# Using variable names to improve clarity
+days_in_a_year = 365
+hours_in_a_day = 24
+minutes_in_an_hour 60
+
+minutes_in_a_year = days_in_a_year * hours_in_a_day * min
