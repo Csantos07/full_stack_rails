284f1292e5f96fb58d3fb0d708a92e10b5e56115
Carlo Santos <rcarlosantos89@gmail.com>
2022-08-29 19:11:03 -0500

    notes for loops and if statements

---
+++ 2_ruby/2_control_flow/2_loops/0_loops.rb
@@ -0,0 +1,32 @@
+# loops allow you to repeat some computation or 
+# conditional logic
+
+# loop
+# while
+# until
+# do while
+# begin
+
+# loops all need some kind of condition to break out 
+# of the repetition
+i = 0
+# loop do 
+#   puts "What is your goal today"
+#   goal = gets.chomp
+#   puts goal
+
+#   if i == 5
+#     break
+#   end
+#   i = i + 1
+# end
+
+while i < 5
+  puts "What is your goal today"
+  goal = gets.chomp
+  puts goal
+
+  i += 1
+end
+
+# https://launchschool.com/books/ruby/read/loops_iterators#simpleloop
