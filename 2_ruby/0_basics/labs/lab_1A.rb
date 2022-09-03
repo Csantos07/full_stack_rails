4d38ba6da6617e521f8ed96dd39dcfe4a1c32d73
Carlo Santos <rcarlosantos89@gmail.com>
2022-09-02 23:02:04 -0500

    add labs

--- 2_ruby/0_basics/labs/lab_1A.rb
+++ 2_ruby/0_basics/labs/lab_1A.rb
@@ -3,3 +3,9 @@
 #
 # Write a small program to calculate how many minutes there are in a year. 
 # Feel free to google the formula if you need help getting started.
+
+days_in_year = 365
+hours_in_a_day = 24
+minutes_in_an_hour = 60
+minutes_in_a_year = minutes_in_an_hour * hours_in_a_day * days_in_year
+puts minutes_in_a_year
--- 2_ruby/0_basics/labs/lab_1B.rb
+++ 2_ruby/0_basics/labs/lab_1B.rb
@@ -2,3 +2,11 @@
 # -------
 #
 # Write a small program to calculate BMI (Body Mass Index)
+
+# BMI = (weight in pounds x 703) / (height in inches x height in inches)
+
+weight_in_pounds = 165.00
+height_in_inches = (4.00 * 12) + 10
+bmi = (weight_in_pounds * 703) / height_in_inches ** 2.00
+
+puts bmi
